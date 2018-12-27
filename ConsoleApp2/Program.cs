b75962ba1faa6d2a0c451b04f5b57f132c9a5512
sadiev <sadiev@itstep.kz>
2018-12-27 20:19:19 +0600

    hgjhg

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,20 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            const int MAX_NUMBERS_IN_ARRAY = 10;
+            int[] numbers = new int[MAX_NUMBERS_IN_ARRAY];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = i;
+            }
+            string[,] names = new string[4, 3];
+            names[0, 0] = "";
+
+            var maxIndexOfRow = names.GetUpperBound(0);
+            DateTime[][] dates = new DateTime[6][];
+            dates[0] = new DateTime[15];
+            dates[0][0] = DateTime.Now;
         }
     }
 }
